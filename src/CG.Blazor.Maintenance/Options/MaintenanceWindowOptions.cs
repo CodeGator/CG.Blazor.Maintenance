3a49565134db1fee8035c61722f31a80ba8e81b3
martycook <martinc@valornet.com>
2021-09-20 14:56:20 -0500

    updated  nuget refs

---
+++ src/CG.Blazor.Maintenance/Options/MaintenanceWindowOptions.cs
@@ -0,0 +1,34 @@
+﻿using CG.Options;
+using System;
+
+namespace CG.Blazor.Maintenance.Options
+{
+    /// <summary>
+    /// This class contains configuration settings related to an automated
+    /// maintenance window.
+    /// </summary>
+    public class MaintenanceWindowOptions : OptionsBase
+    {
+        // *******************************************************************
+        // Properties.
+        // *******************************************************************
+
+        #region Properties
+
+        /// <summary>
+        /// This property contains an optional CRON expression for scheduling 
+        /// an automated maintenance window. If this property isn't specified,
+        /// no maintenance window is opened.
+        /// </summary>
+        public string CRON { get; set; }
+
+        /// <summary>
+        /// This property contains an optional duration for an automated 
+        /// maintenance window. If this property isn't specified, it defaults
+        /// to a fifteen minute duration.
+        /// </summary>
+        public TimeSpan? Duration { get; set; }
+
+        #endregion
+    }
+}
